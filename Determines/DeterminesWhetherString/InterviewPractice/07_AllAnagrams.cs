138f58a2a9f4b303bc3dd87b2603c60d320fb0c9
sergeysud <sergeysud@wizardsdev.com>
2022-04-13 22:37:16 +0300

    add annagram

---
+++ Determines/DeterminesWhetherString/InterviewPractice/07_AllAnagrams.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPractice
+{
+    public class _07_AllAnagrams
+    {
+        public static bool AllAnagrams(string [] str) 
+        {
+            string[] anagramm = new string[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                var ss = str[i].ToCharArray();
+                Array.Sort(ss);
+                anagramm[i] = new string(ss);
+            }
+
+            for (int i = 1; i < anagramm.Length; i++)
+            {
+                if (!anagramm[0].Equals(anagramm[i]))
+                {
+                    return false;
+                }
+            }
+            return true; 
+        }
+    }
+}
---
+++ Determines/DeterminesWhetherString/TestInterviewPractice/Test_07_AllAnagrams.cs
@@ -0,0 +1,28 @@
+﻿using InterviewPractice;
+using Xunit;
+
+namespace TestInterviewPractice
+{
+    public class Test_07_AllAnagrams
+    {
+        [Fact]
+        public void Test0()
+        {
+            string[] rr1 = { "abcd", "bdac", "cabd" };
+            
+
+            var act = _07_AllAnagrams.AllAnagrams(rr1);
+            Assert.True(act);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            string[] rr1 = { "abcd", "bdXc", "cabd" };
+
+
+            var act = _07_AllAnagrams.AllAnagrams(rr1);
+            Assert.False(act);
+        }
+    }
+}
